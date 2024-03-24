0d4ce77b1cba9535981f7c29ce2a73e2a0fe9f6e
DanielPena229185 <daniel.pena229185@potros.itson.edu.mx>
2024-03-23 19:32:32 -0700

    Proyecto
    Por el momento solo tiene el login sin ninguna lógica

---
+++ app/src/main/java/daniel/pena/garcia/grupo_jima_repartidores_app/ui/login/Login.kt
@@ -0,0 +1,21 @@
+package daniel.pena.garcia.grupo_jima_repartidores_app.ui.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import daniel.pena.garcia.grupo_jima_repartidores_app.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
