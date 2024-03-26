3ae0a21b517b8f88bba3fd329f5ebc730b26bc4a
DanielPena229185 <daniel.pena229185@potros.itson.edu.mx>
2024-03-25 22:18:17 -0700

    Iniciar sesion con firebase
    Ya se puede iniciar sesion con firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,6 +41,12 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,6 +56,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/daniel/pena/garcia/grupo_jima_repartidores_app/ui/login/Login.kt
+++ app/src/main/java/daniel/pena/garcia/grupo_jima_repartidores_app/ui/login/Login.kt
@@ -1,13 +1,24 @@
 package daniel.pena.garcia.grupo_jima_repartidores_app.ui.login
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import daniel.pena.garcia.grupo_jima_repartidores_app.R
 
 class Login : AppCompatActivity() {
+    lateinit var btn_sign_in: Button;
+    lateinit var btn_sign_up: Button;
+    lateinit var btn_sign_in_google: Button;
+    lateinit var auth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +27,69 @@ class Login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
+        auth = Firebase.auth;
+        btn_sign_in.setOnClickListener{
+            signIn();
+        }
+    }
+
+    private fun signIn(){
+        val etEmail = findViewById<EditText>(R.id.et_email);
+        val etPassword = findViewById<EditText>(R.id.et_password);
+        val email = etEmail.text.toString();
+        val password = etPassword.text.toString();
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Successful", "signInWithEmail:success")
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Error", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        } else {
+            Toast.makeText(
+                baseContext,
+                "Ningún campo debe estar vacío",
+                Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private fun signUp(){
+        val etEmail = findViewById<EditText>(R.id.et_email);
+        val etPassword = findViewById<EditText>(R.id.et_password);
+        val email = etEmail.text.toString();
+        val password = etPassword.text.toString();
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                if(task.isSuccessful){
+                    Log.d("Successful", "createUserWithEmail:success")
+                }else{
+                    // If sign in fails, display a message to the user.
+                    Log.w("Error", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+        } else {
+            Toast.makeText(
+                baseContext,
+                "Ningún campo debe estar vacío",
+                Toast.LENGTH_SHORT).show();
         }
     }
 }
